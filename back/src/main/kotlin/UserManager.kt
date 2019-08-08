c09cb0a9e8fb590ecff1799ff6bbeb1f73a96b87
Anthony Poncet <anthonyponcet9@gmail.com>
2019-08-08 21:20:03 +0200

    Friend want to buy
    * Friend could have interest in gift

--- back/src/main/kotlin/UserManager.kt
+++ back/src/main/kotlin/UserManager.kt
@@ -4,6 +4,8 @@ import kotlin.Exception
 data class User(val id: Long, val name: String)
 data class Gift(val id: Long, val name: String, val description: String?, val price: String?, val whereToBuy: String?, val categoryId: Long)
 data class Gifts(val gifts: List<Gift>)
+data class FriendGift(val gift: Gift, val interestedUser: List<String>, val buyActionUser: Map<String, BuyAction>)
+data class FriendGifts(val friendGifts: List<FriendGift>)
 data class Category(val id: Long, val name: String)
 data class Categories(val categories: List<Category>)
 data class FriendRequest(val id: Long, val from: User, val to: User, val status: RequestStatus)
@@ -15,7 +17,24 @@ data class RestGift(val name: String?, val description: String?, val price: Stri
 data class RestCategory(val name: String?)
 data class RestCreateFriendRequest(val name: String?)
 
-class UserManager(private var databaseManager: DatabaseManager) {
+/**
+ * No cleaning notion, take care of its validity
+ */
+class DummyUserCache(private val databaseManager: DatabaseManager) {
+    private val cache = HashMap<Long, String>()
+
+    fun query(userId: Long) : String {
+        val name = cache[userId]
+        return if (name != null) name
+        else {
+            val dbName = databaseManager.getUser(userId)
+            cache[userId] = dbName ?: "Unknown"
+            cache[userId]!!
+        }
+    }
+}
+
+class UserManager(private val databaseManager: DatabaseManager) {
 
     fun connect(connectionInformation: ConnectionInformation): User {
         if (connectionInformation.name == null) throw Exception("Username could not be null")
@@ -36,8 +55,20 @@ class UserManager(private var databaseManager: DatabaseManager) {
         return Gifts(databaseManager.getUserGifts(userId).map { g -> Gift(g.id, g.name, g.description, g.price, g.whereToBuy, g.categoryId) })
     }
 
-    fun getFriendGifts(userId: Long, friendName: String): Gifts {
-        return Gifts(databaseManager.getFriendGifts(userId, friendName).map { g -> Gift(g.id, g.name, g.description, g.price, g.whereToBuy, g.categoryId) })
+    //Not optimal at all!
+    fun getFriendGifts(userId: Long, friendName: String): FriendGifts {
+        val gifts = databaseManager.getFriendGifts(userId, friendName).map { g -> Gift(g.id, g.name, g.description, g.price, g.whereToBuy, g.categoryId) }
+
+        val friendGifts = arrayListOf<FriendGift>()
+        val dummyUserCache = DummyUserCache(databaseManager) //Cache only by call
+        for (gift: Gift in gifts) {
+            val actions = databaseManager.getFriendActionOnGift(gift.id)
+            friendGifts.add(FriendGift(gift,
+                actions.filter { it.interested || dummyUserCache.query(it.userId) == "Unknown" }.map { dummyUserCache.query(it.userId) },
+                actions.filter { it.buy != BuyAction.NONE  || dummyUserCache.query(it.userId) == "Unknown" }.map { dummyUserCache.query(it.userId) to it.buy }.toMap() ))
+        }
+
+        return FriendGifts(friendGifts)
     }
 
     fun addGift(userId: Long, gift: RestGift) {
@@ -50,6 +81,34 @@ class UserManager(private var databaseManager: DatabaseManager) {
 
     fun removeGift(userId: Long, giftId: Long) {
         databaseManager.removeGift(userId, giftId)
+    }
+
+    fun interested(giftId: Long, userId: Long, interested: Boolean) {
+        databaseManager.interested(giftId, userId, interested)
+    }
+
+    fun interested(giftId: Long, userId: Long) {
+        databaseManager.interested(giftId, userId, true)
+    }
+
+    fun notInterested(giftId: Long, userId: Long) {
+        databaseManager.interested(giftId, userId, false)
+    }
+
+    fun buy(giftId: Long, userId: Long, buy: BuyAction) {
+        databaseManager.buyAction(giftId, userId, buy)
+    }
+
+    fun stopBuy(giftId: Long, userId: Long) {
+        databaseManager.buyAction(giftId, userId, BuyAction.NONE)
+    }
+
+    fun wantToBuy(giftId: Long, userId: Long) {
+        databaseManager.buyAction(giftId, userId, BuyAction.WANT_TO_BUY)
+    }
+
+    fun hasBought(giftId: Long, userId: Long) {
+        databaseManager.buyAction(giftId, userId, BuyAction.BOUGHT)
     }
 
     fun getUserCategories(userId: Long): Categories {
@@ -107,7 +166,7 @@ class UserManager(private var databaseManager: DatabaseManager) {
     private fun toFriendRequest(dbRequest: DbFriendRequest) : FriendRequest {
         val one = databaseManager.getUser(dbRequest.userOne)!!
         val two = databaseManager.getUser(dbRequest.userTwo)!!
-        return FriendRequest(dbRequest.id, User(one.id, one.name), User(two.id, two.name), dbRequest.status)
+        return FriendRequest(dbRequest.id, User(dbRequest.userOne, one), User(dbRequest.userTwo, two), dbRequest.status)
     }
 
 }
--- front/src/component/FriendWishList.tsx
+++ front/src/component/FriendWishList.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { RouteComponentProps } from "react-router";
 
+import Octicon, {Heart, Checklist, Gift} from '@primer/octicons-react'
+
 import { connect } from 'react-redux';
 import { AppState } from '../redux/store';
 
 import './card-gift.css';
 
 interface PathParam { friendName: string };
-interface ConnectProps { userId: number | null };
+interface ConnectProps { userId: number | null, username: String | null };
 interface Props extends RouteComponentProps<PathParam>, ConnectProps {}
 interface State {
     gifts: any[],
@@ -36,11 +38,61 @@ class FriendWishList extends React.Component<Props, State> {
         const response = await fetch('http://localhost:8080/users/' + userId + '/gifts/' + friendName);
         const json = await response.json();
         if (response.status === 200) {
-            this.setState({ gifts: json.gifts });
+            this.setState({ gifts: json.friendGifts });
         } else {
             console.log(json.error);
         }
     };
+
+    async interested(userId: number | null, giftId: number, imInterested: boolean) {
+        if (userId === null) return; //Impossible
+
+        const response = await fetch('http://localhost:8080/users/' + userId + '/gifts/' + giftId + '/interested?userId=' + userId, {method: imInterested ? "DELETE" : "POST"});
+        if (response.status === 202) {
+            this.getGifts(userId, this.friendName);
+        } else {
+            const json = await response.json();
+            console.log(json.error);
+        }
+    }
+
+    async wantToBuy(userId: number | null, giftId: number, iWantToBuy: boolean, iBought: boolean) {
+        if (userId === null) return; //Impossible
+
+        if (iBought) await this.bought(userId, giftId, false, true); //Remove ibought
+
+        let response = null;
+        if (iWantToBuy === true) {
+            response = await fetch('http://localhost:8080/users/' + userId + '/gifts/' + giftId + '/buy-action?userId=' + userId, {method: "DELETE"});
+        } else {
+            response = await fetch('http://localhost:8080/users/' + userId + '/gifts/' + giftId + '/buy-action?userId=' + userId + '&action=WANT_TO_BUY', {method: "POST"});
+        }
+        if (response.status === 202) {
+            this.getGifts(userId, this.friendName);
+        } else {
+            const json = await response.json();
+            console.log(json.error);
+        }
+    }
+
+    async bought(userId: number | null, giftId: number, iWantToBuy: boolean, iBought: boolean) {
+        if (userId === null) return; //Impossible
+
+        if (iWantToBuy) await this.wantToBuy(userId, giftId, true, false); //Remove ibought
+
+        let response = null;
+        if (iBought === true) {
+            response = await fetch('http://localhost:8080/users/' + userId + '/gifts/' + giftId + '/buy-action?userId=' + userId, {method: "DELETE"});
+        } else {
+            response = await fetch('http://localhost:8080/users/' + userId + '/gifts/' + giftId + '/buy-action?userId=' + userId + '&action=BOUGHT', {method: "POST"});
+        }
+        if (response.status === 202) {
+            this.getGifts(userId, this.friendName);
+        } else {
+            const json = await response.json();
+            console.log(json.error);
+        }
+    }
 
     async getCategories(userId: number, friendName: string)  {
         const response = await fetch('http://localhost:8080/users/' + userId + '/categories/' + friendName);
@@ -64,17 +116,39 @@ class FriendWishList extends React.Component<Props, State> {
       if (this.state.categories) {
         let out = [];
         for (const [index, value] of this.state.categories.entries()) {
-          const cat = (
-              <h5 key={index + value.name}>{value.name}</h5>);
-          let filtered = this.state.gifts.filter(g => { return g.categoryId === value.id });
+          const cat = (<h5 key={index + value.name}>{value.name}</h5>);
+          let filtered = this.state.gifts.filter(g => { return g.gift.categoryId === value.id });
           if (filtered.length === 0) {
               out.push(<>{cat}<p key={index + 'no_gift'}>No gift</p></>);
           }
           else {
-              let giftsOut = filtered.map((gift, gIndex) => {
+              let giftsOut = filtered.map((fGift, gIndex) => {
+                const { gift, interestedUser, buyActionUser } = fGift;
+                let wantToBuy: string[] = [];
+                let bought: string[] = [];
+                Object.keys(buyActionUser).forEach(key => {
+                    if (buyActionUser[key] === "WANT_TO_BUY") wantToBuy.push(key);
+                    if (buyActionUser[key] === "BOUGHT") bought.push(key);
+                });
                 if (index+'-'+gIndex === this.state.hoverId) {
+                  let imInterested = false;
+                  let iWantToBuy = false;
+                  let iBought = false;
+                  if (this.props.username !== null) {
+                      for (const [index, value] of interestedUser.entries()) { if (value === this.props.username) imInterested = true; }
+                      for (const [index, value] of wantToBuy.entries()) { if (value === this.props.username) iWantToBuy = true; }
+                      for (const [index, value] of bought.entries()) { if (value === this.props.username) iBought = true; }
+                  }
                   return (
                       <div className="mycard" onMouseEnter={() => this.handleEnter(index, gIndex)} onMouseLeave={() => this.handleOut()}>
+                          <div className="card-edit-close">
+                            <span className={imInterested ? "text-right icon-selected" : "text-right"} style={{cursor: "pointer"}} onClick={() => this.interested(this.props.userId, gift.id, imInterested)}><Octicon icon={Heart}/></span>{' '}
+                            {interestedUser.length !== 0 && <><span>{interestedUser.length}</span>{' '}</>}
+                            <span className={iWantToBuy ? "icon-selected" : ""} style={{cursor: "pointer"}} onClick={() => this.wantToBuy(this.props.userId, gift.id, iWantToBuy, iBought)}><Octicon icon={Checklist}/></span>{' '}
+                            {wantToBuy.length !== 0 && <><span>{wantToBuy.length}</span>{' '}</>}
+                            <span className={iBought ? "icon-selected" : ""} style={{cursor: "pointer"}} onClick={() => this.bought(this.props.userId, gift.id, iWantToBuy, iBought)}><Octicon icon={Gift}/></span>{' '}
+                            {bought.length !== 0 && <><span>{bought.length}</span>{' '}</>}
+                          </div>
                           <div className="card-name">{gift.name}</div>
                           <div className="card-description">{gift.description}</div>
                           <div className="mycard-footer">
@@ -106,5 +180,5 @@ class FriendWishList extends React.Component<Props, State> {
     }
 }
 
-function mapStateToProps(state: AppState): ConnectProps {return { userId: state.signin.userId };}
+function mapStateToProps(state: AppState): ConnectProps {return { userId: state.signin.userId, username: state.signin.username };}
 export default withRouter(connect(mapStateToProps)(FriendWishList));
