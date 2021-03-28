b657c0b2bd85d407c556973bc9c7660f2064aa1e
Anthony Poncet <anthonyponcet9@gmail.com>
2021-03-28 16:38:22 +0200

    maint(back): bump to ktor 1.5.2 & kotlin 1.4.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.4.20" apply false
+    kotlin("jvm") version "1.4.21" apply false
     id("com.github.johnrengelman.shadow") version "5.2.0" apply false
 }
 
