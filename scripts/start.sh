3bd7c060ed4a6d97f06b8228b82942c67a037ddc
Anthony Poncet <anthonyponcet9@gmail.com>
2021-03-28 16:38:37 +0200

    maint(back): Add logging configuration

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-nohup java -jar auth-server-0.1-all.jar > auth-server.log 2>&1 &
+nohup java -Dlogback.configurationFile=logback/logback.xml -jar auth-server-0.1-all.jar > auth-server.log 2>&1 &
 echo $! > saved_pid.txt
 
-nohup java -jar back-0.1-all.jar > main-server.log 2>&1 &
+nohup java -Dlogback.configurationFile=logback/logback.xml -jar back-0.1-all.jar > main-server.log 2>&1 &
 echo $! >> saved_pid.txt
