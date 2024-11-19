30872fbec2cf3e9eb1f85e360dc1e833635178e4
Anthony Poncet <anthonyponcet9@gmail.com>
2024-11-19 21:17:35 +0000

    cache image

--- front/src/component/SquareImage.tsx
+++ front/src/component/SquareImage.tsx
@@ -27,30 +27,36 @@ function loadImage(
     return;
   }
 
-  const request = async () => {
-    const response = await fetch(url + "/files/" + imageName, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    if (response.status === 404) {
-      console.error("file '" + imageName + "' could not be found on server");
-      return;
-    }
-    if (response.status === 401) {
-      console.error("Unauthorized. Disconnect and redirect to connect");
-      appDispatch(logout());
-      return;
-    }
-    if (response.status === 500) {
-      console.error("Internal server error: " + response);
-      return;
-    }
+  let imageBlob = localStorage.getItem(imageName);
+  if (imageBlob === null) {
+    const request = async () => {
+      const response = await fetch(url + "/files/" + imageName, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (response.status === 404) {
+        console.error("file '" + imageName + "' could not be found on server");
+        return;
+      }
+      if (response.status === 401) {
+        console.error("Unauthorized. Disconnect and redirect to connect");
+        appDispatch(logout());
+        return;
+      }
+      if (response.status === 500) {
+        console.error("Internal server error: " + response);
+        return;
+      }
 
-    response.blob().then((blob) => {
-      let url = window.URL.createObjectURL(blob);
-      setImage(url);
-    });
-  };
-  request();
+      response.blob().then((blob) => {
+        let url = window.URL.createObjectURL(blob);
+        localStorage.setItem(imageName, url);
+        setImage(url);
+      });
+    };
+    request();
+  } else {
+    setImage(imageBlob);
+  }
 }
 
 function SquareImage(props: Props) {
