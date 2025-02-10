962e175b95ed9f6d0b5974a347eaf83f53bd7a95
Anthony Poncet <anthonyponcet9@gmail.com>
2025-02-10 16:09:18 +0000

    more commands

---
+++ src/commands.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    /// Sets a custom config file
+    #[arg(short, long)]
+    pub config: Option<PathBuf>,
+
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Server,
+    CleanPictures,
+    ResetPassword {
+        #[arg(short, long)]
+        name: String,
+    }
+}
